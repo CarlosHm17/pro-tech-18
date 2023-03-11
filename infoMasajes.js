638a7780a5db582bd041d7dc05104ff46edeb1dd
CarlosHm17 <candreshm03@gmail.com>
2023-03-11 09:42:44 -0500

    masajeTrece

---
+++ infoMasajes.js
@@ -0,0 +1,7 @@
+let masajeTrece = document.getElementById("masajeTrece");
+
+masajeTrece.addEventListener("click",infoTrece);
+
+function infoTrece() {
+    alert("Calentamiento muscular - utilizar 10min antes de cada entrenamiento:\nEste modo está diseñado para aumentar la temperatura de los ligamentos musculares y articulaciones. Puede extender el rango y la capacidad de las articulaciones y los ligamentos, también puede reducir la viscosidad muscular y mejorar la contracción muscular y la flexibilidad. El calentamiento muscular es clave para optimizar la calidad del entrenamiento y prevenir lesiones en los deportes.");
+}
