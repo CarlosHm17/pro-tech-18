4417870fd185f64beb9f7c834bf0077729cf7c9c
CarlosHm17 <candreshm03@gmail.com>
2023-03-21 11:41:12 -0400

    info de masajes: 1, 2, 3 y 4

--- infoMasajes.js
+++ infoMasajes.js
@@ -1,3 +1,40 @@
+//Masaje Uno
+let masajeUno = document.getElementById("masajeUno");
+
+masajeUno.addEventListener("click",infoUno);
+
+function infoUno() {
+    alert("Amasado:\nSimula los dedos pulgares y las palmas de un verdadero terapeuta de masaje. La sensación debería sentirse como una vibración. Se enfoca en la hipertensión y el estrés y ayuda a descomponerlo, devolviendo el rango completo de movimiento y flexibilidad.");
+}
+
+//Masaje Dos
+let masajeDos = document.getElementById("masajeDos");
+
+masajeDos.addEventListener("click",infoDos);
+
+function infoDos() {
+    alert("Acupresión:\nUn pulso mucho más rápido y enfocado que se concentra en cualquier nudo o adhesión en el cuerpo. Pone suficiente presión sobre él para descomponerlo y liberar cualquier contenido de ácido láctico.");
+}
+
+//Masaje Tres
+let masajeTres = document.getElementById("masajeTres");
+
+masajeTres.addEventListener("click",infoTres);
+
+function infoTres() {
+    alert("Tapping:\nSimula un segundo latido del corazón. Al tocar el músculo, aumenta el flujo sanguíneo natural del cuerpo y el suministro de oxígeno hacia el área objetivo. Esto también ayuda a eliminar cualquier inflamación o hinchazón de esa área.");
+}
+
+//Masaje Cuatro
+let masajeCuatro = document.getElementById("masajeCuatro");
+
+masajeCuatro.addEventListener("click",infoCuatro);
+
+function infoCuatro() {
+    alert("Ventosas:\nEsto alcanzará, agarrará y estirará el músculo. Ayuda a alargarlo y ampliarlo, liberándolo a medida que retrocede. Esto ayudará a aumentar la flexibilidad y la movilidad del músculo.");
+}
+
+
 
 //Masaje Trece
 let masajeTrece = document.getElementById("masajeTrece");
