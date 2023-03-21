9556eefdaa3b7c38163fcffef649f7c8ba425363
CarlosHm17 <candreshm03@gmail.com>
2023-03-21 12:49:35 -0400

    info masajes

--- infoMasajes.js
+++ infoMasajes.js
@@ -34,7 +34,77 @@ function infoCuatro() {
     alert("Ventosas:\nEsto alcanzará, agarrará y estirará el músculo. Ayuda a alargarlo y ampliarlo, liberándolo a medida que retrocede. Esto ayudará a aumentar la flexibilidad y la movilidad del músculo.");
 }
 
+//Masaje Cinco
+let masajeCinco = document.getElementById("masajeCinco");
 
+masajeCinco.addEventListener("click",infoCinco);
+
+function infoCinco() {
+    alert("Piedra caliente:\nModo de relajación. Esto simula un masaje rodante manual regular que ayuda a liberar y relajar el tejido blando.");
+}
+
+//Masaje Seis
+let masajeSeis = document.getElementById("masajeSeis");
+
+masajeSeis.addEventListener("click",infoSeis);
+
+function infoSeis() {
+    alert("Aleatorio:\nEsta es una combinación de modos 1,4,5. Este es un gran masaje para usar cuando se enfría después de un ejercicio. Codo presionando: Este modo simula al terapeuta de masaje presionando el cuerpo con el codo. Está diseñado para aliviar los dolores de espasmo muscular en el hombro y la espalda.");
+}
+
+//Masaje Siete
+let masajeSiete = document.getElementById("masajeSiete");
+
+masajeSiete.addEventListener("click",infoSiete);
+
+function infoSiete() {
+    alert("Codo presionado:\nEste modo simula al terapeuta de masaje presionando el cuerpo con el codo. Está diseñado para aliviar los dolores de espasmo muscular en el hombro y la espalda.");
+}
+
+//Masaje Ocho
+let masajeOcho = document.getElementById("masajeOcho");
+
+masajeOcho.addEventListener("click",infoOcho);
+
+function infoOcho() {
+    alert("Punto de activación:\nSimula a un terapeuta de masaje presionando los puntos gatillo a lo largo de los meridianos con la punta de los dedos. Se usa para aliviar la fatiga, mantenerse sano y enérgico, y para relajar los tendones.");
+}
+
+//Masaje Nueve
+let masajeNueve = document.getElementById("masajeNueve");
+
+masajeNueve.addEventListener("click",infoNueve);
+
+function infoNueve() {
+    alert("Reflexología:\nEste modo ha sido diseñado para los accesorios del pie. Simula una sesión de reflexología utilizando varios patrones de estimulación diferentes.");
+}
+
+//Masaje Dies
+let masajeDies = document.getElementById("masajeDies");
+
+masajeDies.addEventListener("click",infoDies);
+
+function infoDies() {
+    alert("Terapia auricular:\nPara usarse correctamente, debe colocarse el accesorio Ear Clips. Simula una onda alfa en el cerebro, que se sabe que ayuda a los patrones de sueño, el comportamiento y la liberación de endorfinas. Además, relajará los músculos suboccipitales y promoverá la vasodilatación, ayudando con migrañas y dolores de cabeza.");
+}
+
+//Masaje Once
+let masajeOnceDoce = document.getElementById("masajeOnce");
+
+masajeOnce.addEventListener("click",infoOnce);
+
+function infoOnce() {
+    alert("Microcorriente (A) / Microcorriente (B):\nEstos dos modos son similares pero diferentes en frecuencia. Microcorriente es bien conocida como 'bioelectricidad humana'. Este modo es para prevenir y tratar enfermedades utilizando la corriente para aumentar la microcirculación en el área. Esto puede ayudar con la hinchazón, el alivio de las articulaciones, el alivio del dolor muscular y el rendimiento. Debido a que la microcorriente es subsensorial, a menudo no se percibe ninguna sensación.");
+}
+
+//Masaje Doce
+let masajeDoce = document.getElementById("masajeDoce");
+
+masajeDoce.addEventListener("click",infoDoce);
+
+function infoDoce() {
+    alert("Microcorriente (A) / Microcorriente (B):\nEstos dos modos son similares pero diferentes en frecuencia. Microcorriente es bien conocida como 'bioelectricidad humana'. Este modo es para prevenir y tratar enfermedades utilizando la corriente para aumentar la microcirculación en el área. Esto puede ayudar con la hinchazón, el alivio de las articulaciones, el alivio del dolor muscular y el rendimiento. Debido a que la microcorriente es subsensorial, a menudo no se percibe ninguna sensación.");
+}
 
 //Masaje Trece
 let masajeTrece = document.getElementById("masajeTrece");
@@ -42,5 +112,50 @@ let masajeTrece = document.getElementById("masajeTrece");
 masajeTrece.addEventListener("click",infoTrece);
 
 function infoTrece() {
-    alert("Calentamiento muscular:\nEste modo está diseñado para aumentar la temperatura de los ligamentos musculares y articulaciones. Puede extender el rango y la capacidad de las articulaciones y los ligamentos, también puede reducir la viscosidad muscular y mejorar la contracción muscular y la flexibilidad. El calentamiento muscular es clave para optimizar la calidad del entrenamiento y prevenir lesiones en los deportes.\n\nConsejo:\n-Utilizar 10min antes de cada sesion de entrenamiento.");
+    alert("Calentamiento muscular:\nEste modo está diseñado para aumentar la temperatura de los ligamentos musculares y articulaciones. Puede extender el rango y la capacidad de las articulaciones y los ligamentos, también puede reducir la viscosidad muscular y mejorar la contracción muscular y la flexibilidad. El calentamiento muscular es clave para optimizar la calidad del entrenamiento y prevenir lesiones en los deportes.");
+}
+
+//Masaje Catorce
+let masajeCatorce = document.getElementById("masajeCatorce");
+
+masajeCatorce.addEventListener("click",infoCatorce);
+
+function infoCatorce() {
+    alert("Refuerzo muscular:\nEste modo puede mejorar la calidad del entrenamiento, ayudar a prevenir lesiones deportivas y fortalecer el músculo, también se puede usar para proporcionar un ejercicio físico adecuado que pueda acelerar la circulación sanguínea de los tejidos musculares lesionados o atróficos y promover la síntesis de proteínas para acelerar la rehabilitación de la atrofia muscular.");
+}
+
+//Masaje Quince
+let masajeQuince = document.getElementById("masajeQuince");
+
+masajeQuince.addEventListener("click",infoQuince);
+
+function infoQuince() {
+    alert("Relaiación muscular:\nEste modo está diseñado para estimular los músculos, liberar tensiones y promover el bienestar general mediante la simulación de un masaje que lo abarca todo.");
+}
+
+//Masaje Dieciseis
+let masajeDieciseis = document.getElementById("masajeDieciseis");
+
+masajeDieciseis.addEventListener("click",infoDieciseis);
+
+function infoDieciseis() {
+    alert("Contracción y retención:\nEste modo es la contracción más larga del dispositivo. Está diseñado para simular un movimiento flexible para ayudar a desarrollar tejido muscular.");
+}
+
+//Masaje Dicecisiete
+let masajeDicecisiete = document.getElementById("masajeDicecisiete");
+
+masajeDicecisiete.addEventListener("click",infoDicecisiete);
+
+function infoDicecisiete() {
+    alert("Onde de rampa:\nEste modo está diseñado para comenzar con el número 3 (tapping). Se hará más rápido y más profundo en el interior del músculo a medida que continúa hasta que se contrae por completo. Entonces permitirá que el músculo se libere. Esto se ha utilizado para aumentar la resistencia y desarrollar resistencia a la fatiga.");
+}
+
+//Masaje Dieciocho
+let masajeDieciocho = document.getElementById("masajeDieciocho");
+
+masajeDieciocho.addEventListener("click",infoDieciocho);
+
+function infoDieciocho() {
+    alert("Tapping y amasado:\nUna mezcla de contracciones rápidas y lentas.");
 }
